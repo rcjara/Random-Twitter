9b37f14d6bf5ff6d10d888febe9bd443b2fb0967
Raul Jara <raul.c.jara@gmail.com>
2010-02-20 11:59:53 -0500

    Made the test slightly more complex.

--- test/MarkovLanguageTest.rb
+++ test/MarkovLanguageTest.rb
@@ -24,21 +24,21 @@ describe MarkovLanguage do
   
   context "another, more complex snippet" do
     before(:each) do
-      @sentence = "This is my VERY tiny, small sentence."
+      @snippet = "This is my VERY tiny, small sentence.  It RULES!"
       @lang = MarkovLanguage.new
-      @lang.add_snippet(@sentence)
+      @lang.add_snippet(@snippet)
     end
     
     it "should have 7 words" do
       @lang.num_words.should == 9
     end
     
-    it "should produce the only sentence it is capable of producing" do
-      @lang.gen_snippet.should == @sentence
+    it "should produce the only snippet it is capable of producing" do
+      @lang.gen_snippet.should == @snippet
     end
     
     it "should have the right words" do
-      @lang.words.sort.should == ["this","is","my","very","small","sentence","tiny",",","."].sort
+      @lang.words.sort.should == ["this","is","my","very","small","sentence","tiny",",",". ","it","rules","! "].sort
     end
   end
 end
