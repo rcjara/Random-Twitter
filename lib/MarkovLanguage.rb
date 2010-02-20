791c129f408b8727d78e86973c8669a388fa21e6
Raul Jara <raul.c.jara@gmail.com>
2010-02-19 21:56:20 -0500

    Progress with the MarkovLanguage, but I must work on MarkovWord a little before proceeding.

--- lib/MarkovLanguage.rb
+++ lib/MarkovLanguage.rb
@@ -1,7 +1,62 @@
+require File.expand_path(File.dirname(__FILE__) + '/MarkovWord')
+
 class MarkovLanguage
   def initialize
-    words = {:begin => []}
+    @words = {:begin => MarkovWord.new(:begin, nil)}
   end
   
+  def words
+    @words.keys.select{|word| word.is_a?(String)}
+  end
+  
+  def num_words
+    @words.length - 1
+  end
+  
+  def add_snippet(snippet)
+    pieces = (snippet + " ").scan(/\S+\b|\S/u)
+    
+    handle_word_pair(:begin, pieces[0])
+    
+    pieces.each_cons(2) do |word_pair|
+      handle_word_pair(word_pair[0], word_pair[1])
+    end
+    
+    @words[pieces[-1].downcase].add_child(nil)
+  end
+  
+  def gen_snippet
+    sentence = []
+    current_word = :begin
+    while current_word = @words[current_word].get_random_child
+      sentence << current_word
+    end
+    sentence[0].capitalize + " " + sentence[1..-1].join(" ")
+  end
+  
+
+  private
+  
+  def handle_word_pair(first_word, second_word)
+    first_markov_word = fetch_word(first_word)
+    second_markov_word = fetch_word(second_word)
+    raise "Warning: This word really should exist by now" unless first_markov_word
+    first_markov_word.add_child(second_word)
+    
+    if second_markov_word
+      second_markov_word.add_parent(first_word, second_word)
+    else 
+      @words[second_word.downcase] = MarkovWord.new(second_word, first_word)
+    end
+  end
+  
+  def fetch_word(ident)
+    return nil unless ident
+    if ident.is_a?(Symbol)
+      @words[ident]
+    else
+      @words[ident.downcase]
+    end
+  end
   
 end
--- test/MarkovLanguageTest.rb
+++ test/MarkovLanguageTest.rb
@@ -1,6 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/RandomTwitterHelper')
-require File.expand_path(File.dirname(__FILE__) + '/../lib/MarkovGenerator')
+require File.expand_path(File.dirname(__FILE__) + '/../lib/MarkovLanguage')
 
-describe MarkovGenerator do
-  
+describe MarkovLanguage do
+  context "a simple snippet" do
+    before(:each) do
+      @sentence = "This is my very small sentence."
+      @lang = MarkovLanguage.new
+      @lang.add_snippet(@sentence)
+    end
+    
+    it "should have 7 words" do
+      @lang.num_words.should == 7
+    end
+    
+    it "should produce the only sentence it is capable of producing" do
+      @lang.gen_snippet.should == @sentence
+    end
+    
+    it "should have the right words" do
+      @lang.words.sort.should == ["this","is","my","very","small","sentence","."].sort
+    end
+  end
 end
